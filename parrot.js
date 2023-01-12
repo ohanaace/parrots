5da2043fd6c0d34125467e7418b8f8ea22c7be19
suelen <sreginaclsilva@gmail.com>
2023-01-12 11:39:59 -0300

    adicionei os atributos de correcao automatica, mas o js ainda está inválido

---
+++ parrot.js
@@ -0,0 +1,21 @@
+//let cards =[];
+//let qtdeDecartas;
+
+//do{
+  //  qtdeDecartas = Number(prompt("Escolha a quantidade de cartas para iniciar o jogo:"))
+    //if((qtdeDecartas < 4) && (qtdeDecartas > 14) && (qtdeDecartas % 2 === 1)){
+      //  alert("Quantidade inválida! Digite um número par entre 4 e 14 para iniciar");
+        //qtdeDecartas = Number(prompt("Digite uma quantidade válida:"))}
+        
+//}
+  //  while((qtdeDecartas < 4) && (qtdeDecartas > 14) && (qtdeDecartas % 2 === 1)){
+        
+    //}
+
+
+//function adicionaCard(){
+  //  for(let i = 0; i < qtdeDecartas; i++){
+    //cards.push(i);
+    //}
+    //return cards;
+//}
