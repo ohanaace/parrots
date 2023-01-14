b286fb76ed79e7da421ec428f2b985ebfdd1ae8d
suelen <sreginaclsilva@gmail.com>
2023-01-14 00:07:50 -0300

    inseri a animação de virar a carta ao clicar, mas ainda falta fazer todo o resto e o verso das imagens não passa pq tá indo pro código como undefined

--- parrot.js
+++ parrot.js
@@ -1,9 +1,9 @@
 let qtdeDecartas;
-let parrots = ["./parrot/bobrossparrot.gif", "./parrot/explodyparrot.gif", "./parrot/fiestaparrot.gif", "./parrot/metalparrot.gif", "./parrot/revertitparrot.gif", "./parrot/tripletsparrot.gif", "./parrot/unicornparrot.gif"]
+let parrots = ["./parrot/bobrossparrot.gif", "./parrot/explodyparrot.gif", "./parrot/fiestaparrot.gif", "./parrot/metalparrot.gif", "./parrot/revertitparrot.gif", "./parrot/tripletsparrot.gif", "./parrot/unicornparrot.gif"];
 const cartasEmbaralhadas = parrots.sort(embaralhaCards);
 const primeiraMetade = cartasEmbaralhadas.slice(0, qtdeDecartas/2);
-const segunddaMetade = [...primeiraMetade];
-const baralhoCompleto = primeiraMetade.concat(segunddaMetade);
+const segundaMetade = [...primeiraMetade];
+const baralhoCompleto = primeiraMetade.concat(segundaMetade);
 const baralhoEmbaralhado = baralhoCompleto.sort(embaralhaCards);
 do{
     qtdeDecartas = Number(prompt("Escolha a quantidade de cartas para iniciar o jogo:"))
@@ -18,24 +18,36 @@ do{
     }
 
 
+function embaralhaCards(){
+    return Math.random() - 0.5;
+}
 function adicionaCard(){
     const elemento = document.querySelector('ul');
     for(let i = 0; i < qtdeDecartas; i++){
         elemento.innerHTML += `      <li onclick="viraCarta(this)"data-test ="card">
-        <img src="./parrot/back.png" class="frente" data-test="face-down-image">
-        <img src= ${baralhoEmbaralhado[i]} class="verso" data-test="face-up-imagge">
+        <img src="./parrot/back.png" class="carta frente" data-test="face-down-image">
+        <img src= ${baralhoEmbaralhado[i]} class="carta verso" data-test="face-up-image">
     </li>`
     }
 }
 adicionaCard();
 
-function embaralhaCards(){
-    return Math.random() - 0.5;
-}
 function viraCarta(cartaVirada){
-    //criar uma class no CSS para rotacionar a carta
+
     //conferir se há uma carta selecionada anteriormente
+
     //ao clicar na segunda carta, virar ela tbm
+    cartaVirada.classList.add('virada');
+  
+
+    //criar uma class no CSS para rotacionar a carta
+
+    }
     //fazer um if(carta1 !== carta2)
     //se o if for verdadeiro, remove a classe de rotação
+
+
+
+function desviraCarta(){
+
 }
