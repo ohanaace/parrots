74710555589bbffff81c380b9c0efaa7b9ecdcc2
suelen <sreginaclsilva@gmail.com>
2023-01-15 13:56:13 -0300

    projeto finalizado que chama né bb?

--- parrot.js
+++ parrot.js
@@ -1,10 +1,10 @@
 let qtdeDecartas;
 let parrots = ["./parrot/bobrossparrot.gif", "./parrot/explodyparrot.gif", "./parrot/fiestaparrot.gif", "./parrot/metalparrot.gif", "./parrot/revertitparrot.gif", "./parrot/tripletsparrot.gif", "./parrot/unicornparrot.gif"];
-const cartasEmbaralhadas = parrots.sort(embaralhaCards);
-const primeiraMetade = cartasEmbaralhadas.slice(0, qtdeDecartas/2);
-const segundaMetade = [...primeiraMetade];
-const baralhoCompleto = primeiraMetade.concat(segundaMetade);
-const baralhoEmbaralhado = baralhoCompleto.sort(embaralhaCards);
+let contador = 0;
+let primeiraCarta = undefined;
+let block = false;
+let jogadas = 0;
+let pares = 0;
 do{
     qtdeDecartas = Number(prompt("Escolha a quantidade de cartas para iniciar o jogo:"))
     if((qtdeDecartas < 4) || (qtdeDecartas > 14) || (qtdeDecartas % 2 === 1)){
@@ -12,11 +12,14 @@ do{
         
     }
         
-}
-    while((qtdeDecartas < 4) || (qtdeDecartas > 14) || (qtdeDecartas % 2 === 1)){
+}while((qtdeDecartas < 4) || (qtdeDecartas > 14) || (qtdeDecartas % 2 === 1));
         
-    }
-
+    
+    const cartasEmbaralhadas = parrots.sort(embaralhaCards);
+    const primeiraMetade = cartasEmbaralhadas.slice(0, qtdeDecartas/2);
+    const segundaMetade = [...primeiraMetade];
+    const baralhoCompleto = primeiraMetade.concat(segundaMetade);
+    const baralhoEmbaralhado = baralhoCompleto.sort(embaralhaCards);
 
 function embaralhaCards(){
     return Math.random() - 0.5;
@@ -33,21 +36,55 @@ function adicionaCard(){
 adicionaCard();
 
 function viraCarta(cartaVirada){
-
+    if(block === true){
+        return;
+    }
+    
     //conferir se há uma carta selecionada anteriormente
+    if(!primeiraCarta){
+        cartaVirada.classList.add('virada');
+        primeiraCarta = cartaVirada;
+        jogadas++;
+        contador++;
+        return 
+    }
 
     //ao clicar na segunda carta, virar ela tbm
     cartaVirada.classList.add('virada');
-  
-
-    //criar uma class no CSS para rotacionar a carta
-
+    contador++;
+    if(contador === 2 && cartaVirada.innerHTML !== primeiraCarta.innerHTML){
+        block = true;
+        desviraCarta(primeiraCarta, cartaVirada);
+        jogadas++
+        primeiraCarta = undefined;
+        contador = 0;
+        return
     }
+    jogadas++;
+    pares++;
+    
+    //criar uma class no CSS para rotacionar a carta
+    primeiraCarta = undefined;
+    contador = 0;
+    setTimeout(gameOver, 500);
+    }
+
     //fazer um if(carta1 !== carta2)
     //se o if for verdadeiro, remove a classe de rotação
 
 
 
-function desviraCarta(){
+function desviraCarta(primeiraCarta, segundaCarta){
+    setTimeout(()=> {
+        primeiraCarta.classList.remove('virada');
+        segundaCarta.classList.remove('virada');
+        block = false;
+    }, 1000
+    )
 
+}
+function gameOver(){
+    if(pares === qtdeDecartas/2){
+        alert(`Você ganhou em ${jogadas} jogadas!`)
+    }
 }
